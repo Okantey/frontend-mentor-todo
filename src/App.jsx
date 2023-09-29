ed7c8180dbe995b11c16f280c9e80a489de5471f
nii_okantey <nunoogideon19@gmail.com>
2023-09-29 16:32:41 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
   const [theme, setTheme] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const endPoint = "/todos";
 
   useEffect(() => {
@@ -27,18 +28,22 @@ const App = () => {
     }
   }, [theme]);
 
-  // useEffect(() => {
-  //   fetchTodos();
-  // }, [todos]);
+  useEffect(() => {
+    fetchTodos();
+  }, [todo]);
 
-  // const fetchTodos = async () => {
-  //   try {
-  //     const response = await axios.get(endPoint);
-  //     setTodos(response);
-  //   } catch (error) {
-  //     console.log(error.message);
-  //   }
-  // };
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get(endPoint);
+      setTodos(response.data);
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 2000);
+    }
+  };
 
   const handleThemeSwitch = () => {
     setTheme(theme === "dark" ? "light" : "dark");
@@ -68,7 +73,7 @@ const App = () => {
   return (
     <React.Fragment>
       <div className="w-screen h-screen flex flex-col overflow-x-hidden">
-        <section className="bg-desktop-header-light dark:bg-desktop-header-dark transition-all w-full flex-[35%] object-cover  grid place-items-center">
+        <section className="bg-desktop-header-light dark:bg-desktop-header-dark transition-all w-full py-16 object-cover  grid place-items-center sticky top-0 left-0 right-0">
           <div className="w-[90%] md:w-[30%] m-auto">
             <div className=" w-full flex items-center justify-between">
               <h1 className="text-2xl font-bold text-white">TODO</h1>
@@ -83,11 +88,15 @@ const App = () => {
             <AddTodo todo={todo} setTodo={setTodo} createTodo={createTodo} />
           </div>
         </section>
-        <section className="bg-white transition-colors dark:bg-VeryDarkBlue dark:transition-colors w-full flex-[65%]">
-          <div className="w-[90%] md:w-[30%] m-auto shadow-lg text-DarkGrayishBlue bg-white dark:bg-VeryDarkBlue dark:text-DarkGrayishBlueDark rounded-md mt-[-3rem]">
-            <h1 className="text-center py-3">All Tasks</h1>
-            <AllTodos todos={todos} />
-          </div>
+        <section className="bg-VeryLightGray h-screen transition-colors dark:bg-VeryDarkBlue dark:transition-colors w-full">
+          <section className="bg-VeryLightGray transition-colors dark:bg-VeryDarkBlue dark:transition-colors w-full">
+            <div className="w-[90%] md:w-[30%] m-auto shadow-lg bg-VeryLightGray dark:bg-VeryDarkBlue rounded-md ">
+              <h1 className="text-center py-3 text-VeryDarkGrayishBlue dark:text-VeryDarkGrayishBlueDark">
+                All Tasks
+              </h1>
+              <AllTodos todos={todos} isLoading={isLoading} />
+            </div>
+          </section>
         </section>
       </div>
     </React.Fragment>
--- src/components/AllTodos.jsx
+++ src/components/AllTodos.jsx
@@ -2,32 +2,41 @@
 import React from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
+import { CircularProgress } from "@mui/material";
 
-const AllTodos = ({ todos }) => {
+const AllTodos = ({ todos, isLoading }) => {
   return (
     <React.Fragment>
-      {todos.map((item) => {
-        return (
-          <div key={item.id}>
-            <div className="flex items-center justify-between px-4 py-4 border-y border-DarkGrayishBlueDark">
-              <div className="flex items-center gap-2">
-                <div className="w-4 h-4 rounded-full border cursor-pointer"></div>
-                <p>{item.name}</p>
-              </div>
-              <div className="flex items-center gap-2">
-                <DeleteIcon
-                  className="text-red-600  cursor-pointer p-1 border border-DarkGrayishBlueDark"
-                  fontSize="large"
-                />
-                <EditIcon
-                  className="text-green-600  cursor-pointer p-1 border border-DarkGrayishBlueDark"
-                  fontSize="large"
-                />
+      {isLoading ? (
+        <div className="w-full text-center">
+          <CircularProgress className="text-BrightBlue" />
+        </div>
+      ) : (
+        todos.map((item) => {
+          return (
+            <div key={item.id}>
+              <div className="flex items-center justify-between px-4 py-4 border-y border-DarkGrayishBlueDark">
+                <div className="flex items-center gap-2">
+                  <div className="w-4 h-4 rounded-full border cursor-pointer"></div>
+                  <p className="text-VeryDarkGrayishBlue dark:text-VeryDarkGrayishBlueDark">
+                    {item.name}
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <DeleteIcon
+                    className="text-red-600  cursor-pointer p-1 border border-DarkGrayishBlueDark"
+                    fontSize="large"
+                  />
+                  <EditIcon
+                    className="text-green-600  cursor-pointer p-1 border border-DarkGrayishBlueDark"
+                    fontSize="large"
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </React.Fragment>
   );
 };
